e33e95c126033879b88b162b0b672c253126657e
ayutac <fly.high.android@gmail.com>
2024-06-23 16:06:38 +0200

    Tested Tag generation.

--- src/main/java/org/abos/gs/core/Items.java
+++ src/main/java/org/abos/gs/core/Items.java
@@ -2,10 +2,11 @@ package org.abos.gs.core;
 
 public interface Items {
 
-    public static final ItemLike BIG_SHARP_STONE = new Item("big_sharp_stone", 2);
-    public static final ItemLike BIG_STONE = new Item("big_stone", 1);
-    public static final ItemLike SMALL_SHARP_STONE = new Item("small_sharp_stone", 1);
-    public static final ItemLike SMALL_STONE = new Item("small_stone", 0);
-    public static final ItemLike STICK = new Item("stick", 0);
+    ItemLike BIG_SHARP_STONE = new Item("big_sharp_stone", 2);
+    ItemLike BIG_STONE = new Item("big_stone", 1);
+    ItemLike SHARP_STICK = new Item("sharp_stick", 1);
+    ItemLike SMALL_SHARP_STONE = new Item("small_sharp_stone", 1);
+    ItemLike SMALL_STONE = new Item("small_stone", 0);
+    ItemLike STICK = new Item("stick", 0);
 
 }
--- src/main/java/org/abos/gs/core/Tags.java
+++ src/main/java/org/abos/gs/core/Tags.java
@@ -1,4 +1,9 @@
 package org.abos.gs.core;
 
+import java.util.Set;
+
 public interface Tags {
+
+    TagLike STICKS = new Tag("sticks", Set.of(Items.STICK, Items.SHARP_STICK));
+
 }
