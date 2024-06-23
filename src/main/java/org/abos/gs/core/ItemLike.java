f8aad67ff6e058e95e19c0e9720264daf4da45bc
ayutac <fly.high.android@gmail.com>
2024-06-23 13:11:58 +0200

    Better translation string.

--- src/main/java/org/abos/gs/core/ItemLike.java
+++ src/main/java/org/abos/gs/core/ItemLike.java
@@ -1,6 +1,9 @@
 package org.abos.gs.core;
 
 import org.abos.common.DescribableTranslatable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ResourceBundle;
 
 /**
  * Describes an item. Implementations of this interface should be immutable.
@@ -9,8 +12,13 @@ public interface ItemLike extends Stuff, DescribableTranslatable {
 
     /**
      * Returns the rarity of an item.
+     *
      * @return the rarity of an item, not negative and not {@code null}
      */
-    Integer getRarity();
+    int getRarity();
 
+    @Override
+    default @NotNull String getTranslation(@NotNull ResourceBundle rb) {
+        return rb.getString(String.format("item.%s", getName()));
+    }
 }
--- src/main/java/org/abos/gs/core/TagLike.java
+++ src/main/java/org/abos/gs/core/TagLike.java
@@ -1,9 +1,17 @@
 package org.abos.gs.core;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ResourceBundle;
 import java.util.function.Predicate;
 
 /**
  * Represent a collection of items bundled together. Implementations of this interface should be immutable.
  */
 public interface TagLike extends Stuff, Predicate<ItemLike> {
+
+    @Override
+    default @NotNull String getTranslation(@NotNull ResourceBundle rb) {
+        return rb.getString(String.format("tag.%s", getName()));
+    }
 }
