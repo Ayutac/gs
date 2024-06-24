1235f4c3837477d52ce8d5b6d6436f74fe14a34a
ayutac <fly.high.android@gmail.com>
2024-06-24 18:28:19 +0200

    Made a real window with Fx.

--- src/main/java/org/abos/gs/gui/fx/FxMenu.java
+++ src/main/java/org/abos/gs/gui/fx/FxMenu.java
@@ -1,11 +1,16 @@
 package org.abos.gs.gui.fx;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class FxMenu extends Application {
     @Override
     public void start(Stage stage) {
+        final StackPane sp = new StackPane();
+        final Scene scene = new Scene(sp, 640, 480);
+        stage.setScene(scene);
         stage.show();
     }
 }
