b3fd9b49ac29b64ab14db7b8c3e949acc386b92e
ayutac <fly.high.android@gmail.com>
2024-06-24 18:25:46 +0200

    Returned to Java 17 to allow JavaFx.

---
+++ src/main/java/org/abos/gs/gui/fx/FxMenu.java
@@ -0,0 +1,11 @@
+package org.abos.gs.gui.fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxMenu extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+}
